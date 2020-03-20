222f54edfc36c02840b7492a5b059a5e5250d468
SaifulShihab <shihabmd1970@gmail.com>
2020-03-21 00:10:16 +0600

    create patient model and login/registration

--- patient/models.py
+++ patient/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class patient(models.Model):
+    email = models.CharField(max_length=254, blank=False)
+    password = models.CharField(max_length=50, blank=False)
+    patient_name = models.CharField(max_length=50)
+    father_name = models.CharField(max_length=50)
+    mother_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=11)
+    age = models.CharField(max_length=10)
--- patient/views.py
+++ patient/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib import messages
+from .models import patient
+def login(request):
+    if request.method == 'POST':
+        user_select = request.POST['selecteduser']
+        if user_select == "doctor":
+            pass
+        elif user_select == "patient":            
+            uemail = request.POST['email']
+            upass = request.POST['password']
+            is_login = patient.objects.filter(email=uemail, password=upass)
+            if is_login:                    
+                    return render(request, 'patient/dashboard.html')
+            else:                
+                messages.success(request, "Invalid credential. Try again..")
+                return render(request, 'base/login.html')
+        else:   
+            pass     
+    else:        
+        return render(request, 'base/login.html')
+def registration(request):
+    if request.method == 'POST':
+        user_select = request.POST['selecteduser']
+        if user_select == "doctor":
+            pass
+        elif user_select == "patient":            
+            uemail = request.POST['email']
+            upass = request.POST['password']
+            acc_create = patient(email=uemail, password=upass)
+            if acc_create.save():
+                    messages.success(request, "Error")
+                    return render(request, 'base/registration.html')
+            else:                
+                messages.success(request, "Patient account created!")
+                return render(request, 'base/registration.html')
+        else:   
+            pass         
+    else:        
+        return render(request, 'base/registration.html')
